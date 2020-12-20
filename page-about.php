b3bb84ddbb0e0985241271fccab93d1acede2a83
Alexander Drach <alexander-drach@mail.ru>
2020-12-20 13:34:13 +0600

    html rent page, about page

--- page-about.php
+++ page-about.php
@@ -1,5 +1,156 @@
 <?php include('header.php') ?>
   <main class="page">
-  <div class="container">страница о нас</div>
+  
+
+    <div class="about">
+    <div class="container">
+      <div class="about-inner">
+        <h2 class="title-wrapper j-center"><span class="title-section">О нас</span></h2>
+        <div class="about-inner__text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ab eveniet suscipit sequi corrupti. Assumenda, nisi! Rem saepe, laborum temporibus adipisci laboriosam hic asperiores vero, id consequuntur ullam unde totam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ab eveniet suscipit sequi corrupti. Assumenda, nisi! Rem saepe, laborum temporibus adipisci laboriosam hic asperiores vero, id consequuntur ullam unde totam.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ab eveniet suscipit sequi corrupti. Assumenda, nisi! Rem saepe, laborum temporibus adipisci laboriosam hic asperiores vero, id consequuntur ullam unde totam.
+          </p>
+        </div><!-- about-inner__text -->
+      </div><!-- about-inner -->
+    </div><!-- container -->
+            
+    </div><!-- about -->
+
+    <div class="about-feature">
+      <div class="container">
+        <div class="about-feature-inner">
+          <h2 class="title-wrapper j-center"><span class="title-section">Заголовок особенностей о нас</span></h2>
+          <div class="about-feature-inner__text">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ab eveniet suscipit sequi corrupti. Assumenda, nisi! Rem saepe, laborum temporibus adipisci laboriosam hic asperiores vero, id consequuntur ullam unde totam.
+            </p>
+            <ul>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+              <li>
+                Lorem ipsum dolor sit amet consectetu
+              </li>
+            </ul>
+          </div><!-- about-feature-inner__text -->
+        </div><!-- about-feature-inner -->
+      </div><!-- container -->            
+    </div><!-- about-feature -->
+
+    <section class="documents">
+    <h2 class="title-wrapper j-center"><span class="title-section">Отчетные документы</span></h2>
+      <div class="documents__desc">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam officia, aliquam fugit labore nulla sequi quisquam omnis molestias sit quidem cumque cupiditate sed corrupti temporibus tempore, ex ea.
+      </div>
+      <ul class="documents__list">
+        <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam 
+        </li>
+        <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam 
+        </li>
+        <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam 
+        </li>
+        <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam 
+        </li>
+        <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus explicabo magnam 
+        </li>
+      </ul>
+      <a href="#">Скачать образец</a>
+    </section><!-- documents -->
+
+    <section class="cooperation">
+      <h2 class="title-wrapper j-center"><span class="title-section">Сотрудничество</span></h2>
+      <p class="cooperation-desc">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati explicabo dolore dolor possimus laboriosam distinctio optio, itaque nemo quisquam assumenda? Harum fugit veniam modi non quia id qui sed? Culpa.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam unde quas earum velit pariatur exercitationem odit ad quae cumque. Praesentium, eum eveniet? Accusantium nemo officiis corrupti magni? Facilis, fuga quo!
+      </p>
+      <p class="cooperation-wy-we">
+        Почему выгодно арендовать автомобиль у нас?
+      </p>
+      <ul class="cooperation-list container">
+
+        <li class="cooperation__item">
+          <div class="cooperation-list__icon">icon</div>
+          <span class="cooperation-list__title">заголовок айтема</span>
+          <p class="cooperation-list__desc">
+            описание айтема
+          </p>
+        </li><!-- cooperation__item -->
+
+        <li class="cooperation__item">
+          <div class="cooperation-list__icon">icon</div>
+          <span class="cooperation-list__title">заголовок айтема</span>
+          <p class="cooperation-list__desc">
+            описание айтема
+          </p>
+        </li><!-- cooperation__item -->
+
+        <li class="cooperation__item">
+          <div class="cooperation-list__icon">icon</div>
+          <span class="cooperation-list__title">заголовок айтема</span>
+          <p class="cooperation-list__desc">
+            описание айтема
+          </p>
+        </li><!-- cooperation__item -->
+
+        <li class="cooperation__item">
+          <div class="cooperation-list__icon">icon</div>
+          <span class="cooperation-list__title">заголовок айтема</span>
+          <p class="cooperation-list__desc">
+            описание айтема
+          </p>
+        </li><!-- cooperation__item -->
+      </ul>
+    </section><!-- cooperation -->
+
+    <section class="guarantee">
+      <h2 class="title-wrapper j-center"><span class="title-section">Мы гарантируем</span></h2>
+      <ul class="list_guarantee">
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        </li>
+        <li>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus
+        </li>
+      </ul>
+    </section><!-- guarantee -->
+
   </main>
   <?php include('footer.php') ?>
--- page-rent.php
+++ page-rent.php
@@ -1,87 +1,76 @@
 <?php include('header.php') ?>
 
 <main class="page">
-    <div class="container">
-        <section class="section-rules">
-            <h2 class="title-wrapper j-center"><span class="title-section">Условия аренды</span></h2>
-            <div class="rules-desc">
-                <div class="rules-desc__text">
-                    <p>
-                        <strong>Расчётный час 13.00</strong> по местному времени, до которого гость обязан либо продлить
-                        проживание, либо освободить квартиру.
-                    </p>
-                    <p>
-                        <strong>Арендатор обязан</strong> сдать квартиру в том же состоянии, что и при заселении (мусор
-                        и посуда убраны, в квартире наведен порядок).
-                    </p>
-                    <p>
-                        <strong>Пребывание домашних животных запрещается,</strong> если это не было предварительно
-                        согласовано.
-                    </p>
-                    <p>
-                        Мы дорожим своей репутацией и состоянием наших квартир, поэтому <strong>для проведения различных
-                            увеселительных мероприятий: дни рождения, вечеринки, праздники и т.п. наши квартиры не
-                            сдаются.</strong> Квартиры нашей компании предназначены исключительно только для проживания.
-                    </p>
-                    <p>
-                        <strong>За оставленные, потерянные и забытые вещи компания/арендодатель и их персонал никакой
-                            ответственности не несет.</strong>
-                    </p>
-
-                </div>
-                <div class="rules-desc__list-wrapper">
-                    <div class="title-wrapper wrapper_attention-title j-center">
-                        <p class="attention-title">Гостям категорически запрещается:</p>
-                    </div>
-                    <ul class="list list_attention-title">
-                        <li class="list-marker">Использовать постельное белье и полотенца не по назначению, а так же
-                            портить его.</li>
-                        <li class="list-marker">Оставлять без контроля открытые водопроводные краны.</li>
-                        <li class="list-marker">Мешать соседям громкими звуками, особенно с 22.00 до 8.00.</li>
-                        <li class="list-marker">В квартире не должно находиться большее количество человек, чем при
-                            заселении (или больше, чем оговорено).</li>
-                        <li class="list-marker">Курение в квартире запрещено (исключительно на балконе, а при его
-                            отсутствии — на улице) <strong>штраф 2000р.</strong></li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-        <div class="block-attention">
+  <div class="container">
+    <section class="section-rules">
+        <h2 class="title-wrapper j-center"><span class="title-section">Условия аренды</span></h2>
+        <div class="rules-desc">
+          <div class="rules-desc__text">
             <p>
-                При нарушении правил проживания (не оговоренное кол-во людей, шум, драки, жалобы соседей и т.п.)
-                ВЫСЕЛЕНИЕ ПРОИСХОДИТ НЕМЕДЛЕННО не зависимо от времени суток, деньги при этом не возвращаются.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
             </p>
-        </div>
-        <ul class="list list_rules container">
-            <li class="list-rules__item">
-                <h3 class="title-wrapper j-center"><span class="title-section">Аренда</span></h3>
-                <p>
-                    В момент заселения в квартиру, клиент обязан удостоверить свою личность предъявив арендодателю
-                    паспорт, в ином случае мы отказываем в заселении. Клиент обязуется, использовать квартиру
-                    исключительно для целей проживания.
-                </p>
-            </li>
-            <li class="list-rules__item">
-                <h3 class="title-wrapper j-center"><span class="title-section">Продление срока аренды</span></h3>
-                <p>
-                    В случае необходимости продлить пребывание в квартире после оплаченного срока, гость должен сообщить
-                    об этом арендодателю. При имеющейся возможности арендодатель удовлетворит просьбу гостя о продлении
-                    проживания, но имеет право предложить другую квартиру, а в случае отсутствия свободных квартир
-                    расторгнуть договор посуточной аренды жилого помещения.
-                </p>
-            </li>
-            <li class="list-rules__item">
-                <h3 class="title-wrapper j-center"><span class="title-section">Окончание аренды</span></h3>
-                <p>
-                    О точном времени убытия гость обязан уведомить арендодателя не менее чем за 2 часа. Арендодатель
-                    прибудет на квартиру за 15 мин до отъезда для проверки сохранности имущества, возврата залоговой
-                    суммы. В случае повреждения (порчи) квартиры, и/ или находящегося в квартире имуществу (мебель,
-                    бытовая техника, аксессуары и иное) по вине гостя, гость возмещает нанесённый ущерб согласно
-                    прейскуранту арендодателя. Убедительно просим Вас незамедлительно сообщать арендодателю, о каких
-                    либо происшествиях, неисправностях в арендуемой квартире.
-                </p>
-            </li>
-        </ul>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
+            </p>
+          </div>
+          <div class="rules-desc__list-wrapper">
+            <div class="title-wrapper wrapper_attention-title j-center">
+                <p class="attention-title">Гостям категорически запрещается:</p>
+            </div>
+            <ul class="list list_attention">
+              <li class="list-marker">
+                Запрет 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </li>
+              <li class="list-marker">
+                Запрет 2 Lorem ipsum dolor sit amet consectetur adipisicing elit ipsum dolor sit amet consectetur adipisicing elit ipsum dolor sit amet consectetur adipisicing elit.
+              </li>
+              <li class="list-marker">
+                Запрет 3 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </li>
+              <li class="list-marker">
+              Запрет 4 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </li>
+              <li class="list-marker">
+              Запрет 5 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </li>
+            </ul>
+          </div>
+      </div>
+    </section>
+    <div class="block-attention">
+      <p>
+        При нарушении правил аренды Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus,
+      </p>
     </div>
+    <ul class="list list_rules container">
+      <li class="list-rules__item">
+        <h3 class="title-wrapper j-center"><span class="title-section">Аренда</span></h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum exercitationem voluptate, modi sit nemo vel voluptatum natus nulla eveniet architecto minus! Voluptatem ducimus, commodi a pariatur labore hic libero eos!
+        </p>
+      </li>
+      <li class="list-rules__item">
+        <h3 class="title-wrapper j-center"><span class="title-section">Продление срока аренды</span></h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia odit doloribus, unde molestiae a aliquid consequatur repellat repudiandae id voluptatem natus qui tempora pariatur nemo aperiam porro quo impedit. Tempore.
+        </p>
+      </li>
+      <li class="list-rules__item">
+        <h3 class="title-wrapper j-center"><span class="title-section">Окончание аренды</span></h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. At, veritatis! Consectetur, amet! Recusandae officia ullam reiciendis aliquam reprehenderit nisi accusantium quo quod! Sit incidunt et voluptatem explicabo dolores esse at!
+        </p>
+      </li>
+    </ul>
+  </div>
 </main>
 <?php include('footer.php') ?>
